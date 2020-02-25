b299c6c57d489d65bec009b6be6a07f3fca44d5f
Benjamin Bouvier <public@benj.me>
2020-02-25 20:00:56 +0100

    fuzz: remove interpretation from main fuzzer
    Interpretation, both before or after regalloc, slow down fuzzing by a
    factor of 25. Just running the register allocation is good enough, to
    start with.

--- fuzz/fuzz_bt_differential.rs
+++ fuzz/fuzz_bt_differential.rs
@@ -10,6 +10,7 @@ fuzz_target!(|func: ir::Func| {
     let num_regs = minira::fuzzing::NUM_REAL_REGS_PER_RC as usize;
     let reg_universe = ir::make_universe(num_regs, num_regs);
 
+    let cloned_func = func.clone();
     func.print("before allocation");
 
     let result = match regalloc::allocate_registers(
@@ -26,4 +27,24 @@ fuzz_target!(|func: ir::Func| {
 
     func.update_from_alloc(result);
     func.print("after allocation");
+
+    let expected_ret_value = ir::run_func(
+      &cloned_func,
+      "Before allocation",
+      &reg_universe,
+      ir::RunStage::BeforeRegalloc,
+    );
+
+    let observed_ret_value = ir::run_func(
+      &func,
+      "After allocation",
+      &reg_universe,
+      ir::RunStage::AfterRegalloc,
+    );
+
+    assert_eq!(
+      expected_ret_value, observed_ret_value,
+      "Incorrect interpreter result: expected {:?}, observed {:?}",
+      expected_ret_value, observed_ret_value
+    );
 });
