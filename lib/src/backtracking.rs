3055eac11c8896b12ec373a65d4dfff3bc1b9f61
Julian Seward <jseward@acm.org>
2020-03-11 09:36:42 +0100

    Backtracking allocator: change VirtualRangePrioQ's implementation from the stand-in linear-array implementation to a production-grade binary-heap based implementation.  This gives significant speedups on very large inputs (functions).
    Note that this may change the resulting allocations, because it sometimes
    changes the order in which virtual live ranges are processed.  Even in the
    worst case the effect is small and only appears to affect the situation where
    there is a lot of spilling.  The resulting allocations will of course still be
    correct.

--- lib/src/backtracking.rs
+++ lib/src/backtracking.rs
@@ -9,6 +9,7 @@
 
 use log::debug;
 use std::cmp::Ordering;
+use std::collections::BinaryHeap;
 use std::fmt;
 
 use crate::analysis::run_analysis;
@@ -366,74 +367,151 @@ fn do_coalescing_analysis<F: Function>(
 }
 
 //=============================================================================
-// The as-yet-unallocated VirtualReg LR prio queue
+// The as-yet-unallocated VirtualReg LR prio queue, |VirtualRangePrioQ|.
 //
 // Relevant methods are parameterised by a VirtualRange env.
 
+// What we seek to do with |VirtualRangePrioQ| is to implement a priority
+// queue of as-yet unallocated virtual live ranges.  For each iteration of the
+// main allocation loop, we pull out the highest-priority unallocated
+// VirtualRange, and either allocate it (somehow), or spill it.
+//
+// The Rust standard type BinaryHeap gives us an efficient way to implement
+// the priority queue.  However, it requires that the queue items supply the
+// necessary cost-comparisons by implementing |Ord| on that type.  Hence we
+// have to wrap up the items we fundamentally want in the queue, viz,
+// |VirtualRangeIx|, into a new type |VirtualRangeIxAndSize| that also carries
+// the relevant cost field, |size|.  Then we implement |Ord| for
+// |VirtualRangeIxAndSize| so as to only look at the |size| fields.
+//
+// There is a small twist, however.  Most virtual ranges are small and so will
+// have a small |size| field (less than 20, let's say).  For such cases,
+// |BinaryHeap| will presumably choose between contenders with the same |size|
+// field in some arbitrary way.  This has two disadvantages:
+//
+// * it makes the exact allocation order, and hence allocation results,
+//   dependent on |BinaryHeap|'s arbitrary-choice scheme.  This seems
+//   undesirable, and given recent shenanigans resulting from |HashMap| being
+//   nondeterministic even in a single-threaded scenario, I don't entirely
+//   trust |BinaryHeap| even to be deterministic.
+//
+// * experimentation with the "qsort" test case shows that breaking ties by
+//   selecting the entry that has been in the queue the longest, rather than
+//   choosing arbitrarily, gives slightly better allocations (slightly less
+//   spilling) in spill-heavy situations (where there are few registers).
+//   When there is not much spilling, it makes no difference.
+//
+// For these reasons, |VirtualRangeIxAndSize| also contains a |tiebreaker|
+// field.  The |VirtualRangePrioQ| logic gives a different value of this for
+// each |VirtualRangeIxAndSize| it creates.  These numbers start off at 2^32-1
+// and decrease towards zero.  They are used as a secondary comparison key in
+// the case where the |size| fields are equal.  The effect is that (1)
+// tiebreaking is made completely deterministic, and (2) it breaks ties in
+// favour of the oldest entry (since that will have the highest |tiebreaker|
+// field).
+//
+// The tiebreaker field will wrap around when it hits zero, but that can only
+// happen after processing 2^32-1 virtual live ranges.  In such cases I would
+// expect that the allocator would have run out of memory long before, so it's
+// academic in practice.  Even if it does wrap around there is no danger to
+// the correctness of the allocations.
+
+// Wrap up a VirtualRangeIx and its size, so that we can implement Ord for it
+// on the basis of the |size| and |tiebreaker| fields.
+//
+struct VirtualRangeIxAndSize {
+  vlrix: VirtualRangeIx,
+  size: u16,
+  tiebreaker: u32,
+}
+impl VirtualRangeIxAndSize {
+  fn new(vlrix: VirtualRangeIx, size: u16, tiebreaker: u32) -> Self {
+    assert!(size > 0);
+    Self { vlrix, size, tiebreaker }
+  }
+}
+impl PartialEq for VirtualRangeIxAndSize {
+  fn eq(&self, other: &Self) -> bool {
+    self.size == other.size && self.tiebreaker == other.tiebreaker
+  }
+}
+impl Eq for VirtualRangeIxAndSize {}
+impl PartialOrd for VirtualRangeIxAndSize {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+impl Ord for VirtualRangeIxAndSize {
+  fn cmp(&self, other: &Self) -> Ordering {
+    match self.size.cmp(&other.size) {
+      Ordering::Less => Ordering::Less,
+      Ordering::Greater => Ordering::Greater,
+      Ordering::Equal => self.tiebreaker.cmp(&other.tiebreaker),
+    }
+  }
+}
+
 struct VirtualRangePrioQ {
-  // The set of as-yet unallocated VirtualRanges.  These are indexes into a
-  // VirtualRange env that is not stored here.  These indices can come and
-  // go.
-  unallocated: Vec<VirtualRangeIx>,
+  // The set of as-yet unallocated VirtualRangeIxs.  These are indexes into a
+  // VirtualRange env that is not stored here.  The VirtualRangeIxs are tagged
+  // with the VirtualRange size and a tiebreaker field.
+  heap: BinaryHeap<VirtualRangeIxAndSize>,
+  tiebreaker_ctr: u32,
 }
 impl VirtualRangePrioQ {
   fn new(vlr_env: &TypedIxVec<VirtualRangeIx, VirtualRange>) -> Self {
-    let mut res = Self { unallocated: Vec::new() };
+    let mut res =
+      Self { heap: BinaryHeap::new(), tiebreaker_ctr: 0xFFFF_FFFFu32 };
     for vlrix in
       VirtualRangeIx::new(0).dotdot(VirtualRangeIx::new(vlr_env.len()))
     {
-      assert!(vlr_env[vlrix].size > 0);
-      res.unallocated.push(vlrix);
+      let to_add = VirtualRangeIxAndSize::new(
+        vlrix,
+        vlr_env[vlrix].size,
+        res.tiebreaker_ctr,
+      );
+      res.heap.push(to_add);
+      res.tiebreaker_ctr -= 1;
     }
     res
   }
 
   #[inline(never)]
   fn is_empty(&self) -> bool {
-    self.unallocated.is_empty()
+    self.heap.is_empty()
   }
 
   #[inline(never)]
   fn len(&self) -> usize {
-    self.unallocated.len()
+    self.heap.len()
   }
 
   // Add a VirtualRange index.
   #[inline(never)]
-  fn add_VirtualRange(&mut self, vlr_ix: VirtualRangeIx) {
-    self.unallocated.push(vlr_ix);
+  fn add_VirtualRange(
+    &mut self, vlr_env: &TypedIxVec<VirtualRangeIx, VirtualRange>,
+    vlrix: VirtualRangeIx,
+  ) {
+    let to_add = VirtualRangeIxAndSize::new(
+      vlrix,
+      vlr_env[vlrix].size,
+      self.tiebreaker_ctr,
+    );
+    self.tiebreaker_ctr -= 1;
+    self.heap.push(to_add);
   }
 
   // Look in |unallocated| to locate the entry referencing the VirtualRange
   // with the largest |size| value.  Remove the ref from |unallocated| and
-  // return the LRIx for said entry.
+  // return the VLRIx for said entry.
   #[inline(never)]
-  fn get_longest_VirtualRange(
-    &mut self, vlr_env: &TypedIxVec<VirtualRangeIx, VirtualRange>,
-  ) -> Option<VirtualRangeIx> {
-    if self.unallocated.len() == 0 {
-      return None;
-    }
-    let mut largestIx = self.unallocated.len(); /*INVALID*/
-    let mut largestSize = 0; /*INVALID*/
-    for i in 0..self.unallocated.len() {
-      let cand_vlrix = self.unallocated[i];
-      let cand_vlr = &vlr_env[cand_vlrix];
-      if cand_vlr.size > largestSize {
-        largestSize = cand_vlr.size;
-        largestIx = i;
+  fn get_longest_VirtualRange(&mut self) -> Option<VirtualRangeIx> {
+    match self.heap.pop() {
+      None => None,
+      Some(VirtualRangeIxAndSize { vlrix, size: _, tiebreaker: _ }) => {
+        Some(vlrix)
       }
     }
-    // We must have found *something* since there was at least one
-    // unallocated VirtualRange still available.
-    debug_assert!(largestIx < self.unallocated.len());
-    debug_assert!(largestSize > 0);
-    // What we will return
-    let res = self.unallocated[largestIx];
-    // Remove the selected |unallocated| entry in constant time.
-    self.unallocated[largestIx] = self.unallocated[self.unallocated.len() - 1];
-    self.unallocated.remove(self.unallocated.len() - 1);
-    Some(res)
   }
 
   #[inline(never)]
@@ -441,7 +519,9 @@ impl VirtualRangePrioQ {
     &self, vlr_env: &TypedIxVec<VirtualRangeIx, VirtualRange>,
   ) -> Vec<String> {
     let mut resV = vec![];
-    for vlrix in self.unallocated.iter() {
+    for VirtualRangeIxAndSize { vlrix, size: _, tiebreaker: _ } in
+      self.heap.iter()
+    {
       let mut res = "TODO  ".to_string();
       res += &format!("{:?} = {:?}", vlrix, &vlr_env[*vlrix]);
       resV.push(res);
@@ -1490,7 +1570,7 @@ pub fn alloc_main<F: Function>(
       debug!("");
     }
 
-    let mb_curr_vlrix = prioQ.get_longest_VirtualRange(&vlr_env);
+    let mb_curr_vlrix = prioQ.get_longest_VirtualRange();
     if mb_curr_vlrix.is_none() {
       break 'main_allocation_loop;
     }
@@ -1648,7 +1728,7 @@ pub fn alloc_main<F: Function>(
             &frag_env,
             &vlr_env,
           );
-          prioQ.add_VirtualRange(*vlrix_to_evict);
+          prioQ.add_VirtualRange(&vlr_env, *vlrix_to_evict);
           // Directly modify bits of vlr_env.  This means we have to abandon
           // the immutable borrow for curr_vlr, but that's OK -- we won't need
           // it again (in this loop iteration).
@@ -1781,7 +1861,7 @@ pub fn alloc_main<F: Function>(
           &frag_env,
           &vlr_env,
         );
-        prioQ.add_VirtualRange(*vlrix_to_evict);
+        prioQ.add_VirtualRange(&vlr_env, *vlrix_to_evict);
         debug_assert!(vlr_env[*vlrix_to_evict].rreg.is_some());
         vlr_env[*vlrix_to_evict].rreg = None;
       }
@@ -1943,7 +2023,7 @@ pub fn alloc_main<F: Function>(
       let new_vlrix = VirtualRangeIx::new(vlr_env.len() as u32);
       debug!("--     new VirtRange    {:?}  :=  {:?}", new_vlrix, &new_vlr);
       vlr_env.push(new_vlr);
-      prioQ.add_VirtualRange(new_vlrix);
+      prioQ.add_VirtualRange(&vlr_env, new_vlrix);
 
       let new_eli = EditListItem {
         slot: next_spill_slot,
