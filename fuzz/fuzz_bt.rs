2bed473e08d8bb03f8b400253c8d4832fb8a9090
Dan Gohman <sunfish@mozilla.com>
2020-04-15 10:01:23 -0700

    rustfmt fuzz source files.

--- fuzz/fuzz_bt.rs
+++ fuzz/fuzz_bt.rs
@@ -20,60 +20,71 @@ static mut COUNTER_GEN: usize = 0;
 static mut COUNTER_OK: usize = 0;
 
 fuzz_target!(|func: ir::Func| {
-  let n_gen = unsafe { COUNTER_GEN += 1; COUNTER_GEN };
-  let n_ok = unsafe { COUNTER_OK };
-  println!("==== BEGIN fuzz_bt.rs: #gen'd {:?} #ok {} ========================",
-           n_gen, n_ok);
+    let n_gen = unsafe {
+        COUNTER_GEN += 1;
+        COUNTER_GEN
+    };
+    let n_ok = unsafe { COUNTER_OK };
+    println!(
+        "==== BEGIN fuzz_bt.rs: #gen'd {:?} #ok {} ========================",
+        n_gen, n_ok
+    );
 
-  if false {
-    println!("BEGIN INPUT:");
-    let mut rendered = String::new();
-    func.render("==== fuzz_bt.rs: failing input:", &mut rendered).unwrap();
-    println!("{}", rendered);
-    println!("END INPUT:");
-  }
-
-  let mut func = func;
-
-  let num_regs = minira::fuzzing::NUM_REAL_REGS_PER_RC as usize;
-  let reg_universe = ir::make_universe(num_regs, num_regs);
-
-  let func_backup = func.clone();
-
-  let ra_result = regalloc::allocate_registers(
-    &mut func,
-    //TODO reenable checking once #47 is fixed.
-    regalloc::RegAllocAlgorithm::Backtracking,
-    &reg_universe,
-    /*request_block_annotations=*/false
-  );
-
-  match ra_result {
-    Ok(result) => {
-      func.update_from_alloc(result);
-      unsafe { COUNTER_OK += 1; }
-      return;
-    }
-    Err(err) => {
-      let mut stop = false;
-      if let regalloc::RegAllocError::RegChecker(_) = &err {
-        stop = true;
-        println!("==== fuzz_bt.rs: checker error: {:?}", err);
-      }
-      if stop {
+    if false {
+        println!("BEGIN INPUT:");
         let mut rendered = String::new();
-        func_backup.render("==== fuzz_bt.rs: failing input:",
-                           &mut rendered).unwrap();
+        func.render("==== fuzz_bt.rs: failing input:", &mut rendered)
+            .unwrap();
         println!("{}", rendered);
-      }
-      println!("==== fuzz_bt.rs: failure reason: {}", err);
-      if stop {
-        println!("==== fuzz_bt.rs:");
-        println!("==== fuzz_bt.rs: to repro, use flags '-f {} -i {}'",
-                num_regs, num_regs);
-        println!("==== fuzz_bt.rs:");
-        panic!("==== fuzz_bt.rs: STOPPING.  Bye! ====");
-      }
+        println!("END INPUT:");
     }
-  };
+
+    let mut func = func;
+
+    let num_regs = minira::fuzzing::NUM_REAL_REGS_PER_RC as usize;
+    let reg_universe = ir::make_universe(num_regs, num_regs);
+
+    let func_backup = func.clone();
+
+    let ra_result = regalloc::allocate_registers(
+        &mut func,
+        //TODO reenable checking once #47 is fixed.
+        regalloc::RegAllocAlgorithm::Backtracking,
+        &reg_universe,
+        /*request_block_annotations=*/ false,
+    );
+
+    match ra_result {
+        Ok(result) => {
+            func.update_from_alloc(result);
+            unsafe {
+                COUNTER_OK += 1;
+            }
+            return;
+        }
+        Err(err) => {
+            let mut stop = false;
+            if let regalloc::RegAllocError::RegChecker(_) = &err {
+                stop = true;
+                println!("==== fuzz_bt.rs: checker error: {:?}", err);
+            }
+            if stop {
+                let mut rendered = String::new();
+                func_backup
+                    .render("==== fuzz_bt.rs: failing input:", &mut rendered)
+                    .unwrap();
+                println!("{}", rendered);
+            }
+            println!("==== fuzz_bt.rs: failure reason: {}", err);
+            if stop {
+                println!("==== fuzz_bt.rs:");
+                println!(
+                    "==== fuzz_bt.rs: to repro, use flags '-f {} -i {}'",
+                    num_regs, num_regs
+                );
+                println!("==== fuzz_bt.rs:");
+                panic!("==== fuzz_bt.rs: STOPPING.  Bye! ====");
+            }
+        }
+    };
 });
